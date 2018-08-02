4091515e41505c6ff68c32983aef986d7bfe7b23
OAKSMITH\nilonapo <nap.nilo@gmail.com>
2018-08-02 20:34:21 +0800

    let the good times roll

---
+++ exercise 3.js
@@ -0,0 +1,20 @@
+﻿function ReverseString(text) {
+
+    var arrText = text.split(' ');
+
+    for (var i = 0; i < arrText.length; i++) {
+        if (i % 2 != 0) {
+            var toRev = arrText[i]
+            var revText = '';
+            for (var x = toRev.length - 1; x >= 0; x--) {
+                revText += toRev[x];
+            }
+
+            arrText[i] = revText;
+        }
+    }
+
+    return arrText.join(' ');
+
+}
+
